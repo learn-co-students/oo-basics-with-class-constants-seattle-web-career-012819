028f6a768d947a06dd3f79a5afedbd5e17166f2a
github username <github email address>
2019-01-29 19:54:48 -0800

    First commit.

--- lib/book.rb
+++ lib/book.rb
@@ -2,6 +2,8 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
+  GENRES = []
+
   def initialize(title)
     @title = title
   end
@@ -10,4 +12,10 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
+  # The reason we are using only a reader macro and a custom setter method
+  # is because we need to modify this method to shovel genres into the class
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
 end
